bb1d8ff13d9954c7db899ab7288200392de8f953
David <dmetraux@isti.com>
2025-11-10 11:51:07 -0500

    style buttons better

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # cs601_final_project
 
 # Deployment
-Run 
+Go to cs601_finalproject
 ```shell
 $ npm run deploy
 ```
@@ -10,3 +10,6 @@ $ npm run deploy
 Used https://github.com/gitname/react-gh-pages to help setup.
 
 Used https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/menu to help with menu styling
+
+Used for Button formatting:
+https://www.w3schools.com/css/css3_buttons.asp
--- cs601_final_project/src/components/Banner.tsx
+++ cs601_final_project/src/components/Banner.tsx
@@ -2,22 +2,35 @@ import { Button } from '@mui/material';
 import '../style/Banner.css'
 import headshot from '../media/headshot_no_bg.png'
 
+const links: {name: string, href: string}[]  = [
+    {name: "One", href: "/one"},
+    {name: "Two", href: "/two"},
+    {name: "Three", href: "/three"}
+]
+
 function Banner(){
+
+    const listLinks = links.map((link)=>{
+        return <li key={link.href}><a href={link.href}>{link.name}</a></li>
+    })
+
     return (
     <div id='banner'>
         <div id='siteTitle'>
             <h1>David Metraux</h1>
             <div id="headShot">
                 <div className='overlay'></div>
-                <img  height="120" src={headshot}></img>
+                <img  height="120" alt='headshot of David Metraux, man with long hair and beard' src={headshot}></img>
             </div>
         </div>
 
         <div>
             <menu id='menu'>
-                <li><button>One</button></li>
+                {/* <li><button>One</button></li>
                 <li><button>Two</button></li>
-                <li><button>Three</button></li>
+                <li><button>Three</button></li> */}
+                {listLinks}
+
             </menu>
         </div>
     </div>
