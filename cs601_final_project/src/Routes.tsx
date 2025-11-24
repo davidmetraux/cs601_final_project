0535064a9c0b1d43e2eb020d5d4e7780552dbf52
David <dmetraux@isti.com>
2025-11-24 15:03:09 -0500

    Form basics

--- cs601_final_project/src/Routes.tsx
+++ cs601_final_project/src/Routes.tsx
@@ -1,6 +1,7 @@
 import { JSX } from "react"
 import Pathfinder from "./pages/Pathfinder"
 import Weather from "./pages/Weather"
+import Contact from "./pages/Contact"
 
 interface RoutesInteface {
   name: string, 
@@ -11,6 +12,7 @@ interface RoutesInteface {
 const routes: RoutesInteface[]  = [
     {name: "Pathfinder", component:<Pathfinder/>, href: "pathfinder"},
     {name: "Weather Near David", component:<Weather/>, href: "weather"},
+    {name:"Contact David", component:<Contact/>, href: "contact"}
 ]
 
 export { routes }
--- cs601_final_project/src/pages/Contact.tsx
+++ cs601_final_project/src/pages/Contact.tsx
@@ -1,17 +1,37 @@
+import '../style/Form.css'
+
 
 function Contact(){
 
     const form = () => {
         return (
-            <form>
-                <button></button>
-            </form>
+            <div className='formRoot'>
+                <h2>Contact David</h2>
+                <form>
+                    <div>
+                        <label>Name</label>
+                        <input type="text"></input>
+                        <p className="error"></p>
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input type="email"></input>
+                        <p className="error"></p>
+                    </div>
+                    <div>
+                        <label>Message</label>
+                        <textarea rows={5}></textarea>
+                        <p className="error"></p>
+                    </div>
+                    <button> Submit </button>
+                </form>
+            </div>
         )
     }
 
 
     return (
-        {form}
+       <>{form()}</>
     )
 }
 
