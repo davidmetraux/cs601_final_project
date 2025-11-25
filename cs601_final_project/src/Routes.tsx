fe150532b5d5dc6c9eb0bca3012b13aff476d4d6
David <dmetraux@isti.com>
2025-11-25 13:06:39 -0500

    styling home screen, getting it attached to routes

--- cs601_final_project/src/Routes.tsx
+++ cs601_final_project/src/Routes.tsx
@@ -7,14 +7,15 @@ import Home from "./pages/Home"
 interface RoutesInteface {
   name: string, 
   component:JSX.Element,
-  href: string
+  href: string,
+  description: string
 }
 
 const routes: RoutesInteface[]  = [
-    {name: "Home", component:<Home/>, href: ""},
-    {name: "Pathfinder", component:<Pathfinder/>, href: "pathfinder"},
-    {name: "Weather Near David", component:<Weather/>, href: "weather"},
-    {name:"Contact David", component:<Contact/>, href: "contact"}
+    {name: "Home", component:<Home/>, href: "", description: ""},
+    {name: "Pathfinder", component:<Pathfinder/>, href: "pathfinder",  description: "Explore David Metraux's experience in Pathfinder!"},
+    {name: "Weather Near David", component:<Weather/>, href: "weather",  description: "See what the weather is like in Boston, where David Metraux lives."},
+    {name:"Contact David", component:<Contact/>, href: "contact",  description: "See some places where you can contact David Metraux, or send him a message."}
 ]
 
 export { routes }
