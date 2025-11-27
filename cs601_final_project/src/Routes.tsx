73e266f0bc1a0e149575ff6cd74f97678153ed10
David <dmetraux@isti.com>
2025-11-26 20:28:47 -0500

    got most of the arrow functions together for Resume page

--- cs601_final_project/src/Routes.tsx
+++ cs601_final_project/src/Routes.tsx
@@ -3,6 +3,7 @@ import Pathfinder from "./pages/Pathfinder"
 import Weather from "./pages/Weather"
 import Contact from "./pages/Contact"
 import Home from "./pages/Home"
+import Resume from "./pages/Resume"
 
 interface RoutesInteface {
   name: string, 
@@ -15,7 +16,8 @@ const routes: RoutesInteface[]  = [
     {name: "Home", component:<Home/>, href: "", description: ""},
     {name: "Pathfinder", component:<Pathfinder/>, href: "pathfinder",  description: "Explore David Metraux's experience in Pathfinder!"},
     {name: "Weather Near David", component:<Weather/>, href: "weather",  description: "See what the weather is like in Boston, where David Metraux lives."},
-    {name:"Contact David", component:<Contact/>, href: "contact",  description: "See some places where you can contact David Metraux, or send him a message."}
+    {name:"Contact David", component:<Contact/>, href: "contact",  description: "See some places where you can contact David Metraux, or send him a message"},
+    {name:"Resume", component:<Resume/>, href: "resume",  description: "See David Metraux's Resume."}
 ]
 
 export { routes }
