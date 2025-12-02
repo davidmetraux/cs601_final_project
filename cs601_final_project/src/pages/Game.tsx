40e355df0ec22b8476a3a075319e4bd6c2322147
David <dmetraux@isti.com>
2025-12-02 14:47:22 -0500

    adding basics

--- cs601_final_project/src/pages/Game.tsx
+++ cs601_final_project/src/pages/Game.tsx
@@ -1,5 +1,20 @@
+import { useRef } from "react"
+import style from '../style/Game.module.css'
+
 function Game(){
-    return (<div>game</div>)
+    const redRef = useRef(null)
+    const greenRef = useRef(null)
+    const yellowRef = useRef(null)
+    const blueRef = useRef(null)
+
+    return (
+        <div className={style.squareHolder}>
+            <div id={style.red} ref={redRef}></div>
+            <div id={style.green} ref={greenRef}></div>
+            <div id={style.yellow} ref={yellowRef}></div>
+            <div id={style.blue} ref={blueRef}></div>
+        </div>
+    )
 }
 
 
